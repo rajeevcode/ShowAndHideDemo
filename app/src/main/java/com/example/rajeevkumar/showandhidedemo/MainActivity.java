0af8511807069711467ffa567b519ba85078de33
Rajeev Kumar <rajeev.code@gmail.com>
2018-03-12 07:53:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rajeevkumar/showandhidedemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.rajeevkumar.showandhidedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+	
+	TextView textView;
+	
+	public void Show(View view){
+	
+		textView.setVisibility(View.VISIBLE);
+		
+	}
+	
+	public void Hide(View view){
+		
+		textView.setVisibility(View.GONE);
+	}
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	
+	textView = findViewById(R.id.textView);
+	
+}
+}
